6dd376aec973e29c7cc66b5183054b6d629d01ee
MohamedAshraf92 <eng.mohamed.hfs@gmail.com>
2020-08-03 00:01:49 +0200

    render car and obstacle

--- control.js
+++ control.js
@@ -1,21 +1,16 @@
-window.onload = () => {
-    requestAnimationFrame(animate)
-    // ctx.drawImage(car, 156, 550)
-}
+
 const canvas = document.querySelector(".canvas")
 const ctx = canvas.getContext("2d")
 
-let ySpeed = -325
-
 road = new Image()
 road.src = "./road.jpg"
+let roadSpeed = -232
 
 car = new Image()
 car.src = "./car.png"
 let position = 116
 
 window.addEventListener('keydown', e => {
-    console.log(e)
     if (e.keyCode === 37) {
         if (position <= 226 && position > 6) {
             position -= 110
@@ -32,19 +27,39 @@ window.addEventListener('keydown', e => {
     }
 })
 
-animate= () => {
-    if (ySpeed >= 0) ySpeed = -325
-    ctx.drawImage(road, 0, ySpeed)
-    ctx.drawImage(road, 0, ySpeed + 325)
-    ctx.drawImage(road, 0, ySpeed + 650)
-    ySpeed += 3
+obstacle = new Image()
+obstacle.src = "./obstacle.png"
+let obstacleSpeed = 0
 
-    ctx.drawImage(car, position, 500)
+setInterval (() => {
+    obstacleSpeed += 2
+}, 5)
 
-    requestAnimationFrame(animate)
+obstacleUpdate = () => {
+    ctx.drawImage(obstacle, 6, obstacleSpeed)
+    console.log('its working')    
 }
 
+setInterval(() => {
+    obstacleUpdate()
+}, 1000);
 
-window.onload()
+animate = () => {
+    if (roadSpeed >= 0) roadSpeed = -232
+    ctx.drawImage(road, 0, roadSpeed)
+    ctx.drawImage(road, 0, roadSpeed + 232)
+    ctx.drawImage(road, 0, roadSpeed + 464)
+    ctx.drawImage(road, 0, roadSpeed + 696)
+    roadSpeed += 2
+    
+    requestAnimationFrame(animate)
+    ctx.drawImage(car, position, 500)
+    ctx.drawImage(obstacle, 6, obstacleSpeed)
+}
+
+animate()
+
+
+
 
 
