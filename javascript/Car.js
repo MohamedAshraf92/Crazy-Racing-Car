cd2cdd299d17a7bcd716c67da11eaf1e799fd8e4
MohamedAshraf92 <eng.mohamed.hfs@gmail.com>
2020-08-10 04:28:19 +0200

    update modules

--- javascript/Car.js
+++ javascript/Car.js
@@ -1,5 +1,8 @@
-let carLane
-let carHeight
+export let carObj = {
+    carLane: 161.5,
+    carHeight: 500
+}
+
 export default () => {
 
     const canvas2 = document.querySelector(".canvas_2")
@@ -7,41 +10,38 @@ export default () => {
     
     const car = new Image()
     car.src = "../img/car.png"
-    let carLane = 161.5
-    let carHeight = 500
+    // let carLane = 161.5
+    // let carHeight = 500
     
-    ctx2.drawImage(car, carLane, carHeight)
+    // ctx2.drawImage(car, carObj.carLane, carObj.carHeight)
 
     window.addEventListener('keydown', e => {
         if (e.keyCode === 37) {
-            if (carLane <= 290.5 && carLane > 32.5) {
-                carLane -= 129
+            if (carObj.carLane <= 290.5 && carObj.carLane > 32.5) {
+                carObj.carLane -= 129
                 ctx2.clearRect(0, 0, 422, 650)
-                ctx2.drawImage(car, carLane, carHeight)
-            } else if (carLane < 32.5) {
-                carLane = 129
+                ctx2.drawImage(car, carObj.carLane, carObj.carHeight)
+            } else if (carObj.carLane < 32.5) {
+                carObj.carLane = 129
                 ctx2.clearRect(0, 0, 422, 650)
-                ctx2.drawImage(car, carLane, carHeight)
+                ctx2.drawImage(car, carObj.carLane, carObj.carHeight)
             }
             
         } else if (e.keyCode === 39) {
-            if (carLane >= 32.5 && carLane < 290.5) {
-                carLane += 129
+            if (carObj.carLane >= 32.5 && carObj.carLane < 290.5) {
+                carObj.carLane += 129
                 ctx2.clearRect(0, 0, 422, 650)
-                ctx2.drawImage(car, carLane, carHeight)
-            } else if (carLane > 290.5) {
-                carLane = 129
+                ctx2.drawImage(car, carObj.carLane, carObj.carHeight)
+            } else if (carObj.carLane > 290.5) {
+                carObj.carLane = 129
                 ctx2.clearRect(0, 0, 422, 650)
-                ctx2.drawImage(car, carLane, carHeight)
+                ctx2.drawImage(car, carObj.carLane, carObj.carHeight)
             }
         }
-        console.log(carLane)
+        console.log(carObj.carLane)
     })
     // return carLane
 }
 
-export const carObj = {
-    carLane: carLane,
-    carHeight: carHeight
-}
+
 
